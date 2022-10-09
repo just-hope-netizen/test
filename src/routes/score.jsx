0c72096ed150c8b68a125a154a871d2e216d7e89
just-hope-netizen <hopepeter007@gmail.com>
2022-10-09 10:23:52 +0100

    Refactor

--- src/routes/score.jsx
+++ src/routes/score.jsx
@@ -21,45 +21,50 @@ function Score() {
 
   }, [scores]);
 
-  
+
   return (
-     <><button
-      className='btn  btn-dark '
-      type='button'
-      onClick={() => navigate('/')}
-    >
-      <ArrowIcon className='animate-icon' />
-      Go back
-    </button><table className='table table-striped table-dark my-5  container'>
-        <thead>
-          <tr>
-            <th scope='col'>#</th>
-            <th scope='col'>Name</th>
-            <th scope='col'>email</th>
-            <th scope='col'>age</th>
-            <th scope='col'>address</th>
-            <th scope='col'>occupation</th>
-            <th scope='col'>currentlyEmployed</th>
-            <th scope='col'>yrsEmployed</th>
-            <th scope='col'>score</th>
-          </tr>
-        </thead>
-        <tbody>
-          {scores?.map((score, index) => (
-            <tr key={index}>
-              <th scope='row'>{index}</th>
-              <td>{score.user?.name}</td>
-              <td>{score.user?.email}</td>
-              <td>{score.user?.age}</td>
-              <td>{score.user?.address}</td>
-              <td>{score.user?.occupation}</td>
-              <td>{score.user?.currentlyEmployed === true ? 'Yes' : 'No'}</td>
-              <td>{score.user?.yrsEmployed}</td>
-              <td>{score.score}</td>
+    <>
+      <button
+        className='btn  btn-dark '
+        type='button'
+        onClick={() => navigate('/')}
+      >
+        <ArrowIcon className='animate-icon' />
+        Go back
+      </button>
+      <div className='table-responsive'>
+        <table className='table table-striped table-dark my-5  container'>
+          <thead>
+            <tr>
+              <th scope='col'>#</th>
+              <th scope='col'>Name</th>
+              <th scope='col'>email</th>
+              <th scope='col'>age</th>
+              <th scope='col'>address</th>
+              <th scope='col'>occupation</th>
+              <th scope='col'>currentlyEmployed</th>
+              <th scope='col'>yrsEmployed</th>
+              <th scope='col'>score</th>
             </tr>
-          ))}
-        </tbody>
-      </table></>
+          </thead>
+          <tbody>
+            {scores?.map((score, index) => (
+              <tr key={index}>
+                <th scope='row'>{index}</th>
+                <td>{score.user?.name}</td>
+                <td>{score.user?.email}</td>
+                <td>{score.user?.age}</td>
+                <td>{score.user?.address}</td>
+                <td>{score.user?.occupation}</td>
+                <td>{score.user?.currentlyEmployed === true ? 'Yes' : 'No'}</td>
+                <td>{score.user?.yrsEmployed}</td>
+                <td>{score.score}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
  
 }
